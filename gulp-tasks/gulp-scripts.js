6ba5a1dd928a8aa083ac67e4046820aa35f4fdd1
xNok <nokwebspace@gmail.com>
2017-07-18 22:01:59 -0400

    clean uo css tasks

--- gulp-tasks/gulp-scripts.js
+++ gulp-tasks/gulp-scripts.js
@@ -4,15 +4,20 @@ import uglify       from 'gulp-uglify';
 import babel        from 'gulp-babel';
 import debug        from 'gulp-debug';
 
-import paths        from '../paths.json';
+import config       from '../config.js';
 
-export function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
+/**
+** GULP tasks dedicated to process scripts files
+****************************/
+
+
+export function scripts_es6() {
+  return gulp.src(config.entry.scripts, { sourcemaps: true })
     .pipe(debug())
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat('scripts.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest));
+    .pipe(gulp.dest(config.output.scripts));
 }
 
-gulp.task('scripts', scripts);
+gulp.task('scripts', scripts_es6);
